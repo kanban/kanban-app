e13c1b16949f2f8a7fc0c603e502a576b5f51f85
Edward Glen <eglen.git@glencomm.com>
2011-07-13 22:02:43 +1200

    Including the accidentally missed DataController unit test.

---
+++ src/test/java/com/metservice/kanban/web/KanbanDataControllerTest.java
@@ -0,0 +1,34 @@
+package com.metservice.kanban.web;
+
+import com.metservice.kanban.KanbanService;
+import java.io.File;
+import java.io.IOException;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang.SystemUtils;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.mock.web.MockHttpServletResponse;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class KanbanDataControllerTest {
+
+    private KanbanService fakeKanbanService;
+    
+    @Before
+    public void setup() throws IOException {
+        fakeKanbanService = new KanbanService(new File(SystemUtils.getUserDir(), "/src/test/resources"));
+    }
+    
+    @Test
+    public void downloadCsv() throws IOException {
+        KanbanDataController dataController = new KanbanDataController(fakeKanbanService);
+        MockHttpServletResponse response = new MockHttpServletResponse();
+        dataController.download("test-project", "story", response);
+        assertThat((String)response.getHeader("Content-Disposition"), is("attachment; filename=\"story.csv\""));
+        assertThat(response.getContentType(), is("text/csv"));
+        File expectedFile = new File(SystemUtils.getUserDir(), "/src/test/resources/test-project/story.csv");
+        String expectedContent = FileUtils.readFileToString(expectedFile);
+        assertThat(response.getContentAsString(), is(expectedContent));
+    }
+}
