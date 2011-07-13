043ff6160baf04f92772b77a8068aab21857e1a6
Edward Glen <eglen.git@glencomm.com>
2011-07-13 22:00:22 +1200

    Simplifying the DataController to download only single csv's.  If you have a multi-layered project, you'll have to download the csv for each layer individually.

--- src/main/java/com/metservice/kanban/web/KanbanDataController.java
+++ src/main/java/com/metservice/kanban/web/KanbanDataController.java
@@ -5,11 +5,14 @@ import java.io.IOException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import com.metservice.kanban.KanbanService;
-import com.metservice.kanban.csv.KanbanCsvFile;
 import com.metservice.kanban.model.KanbanProjectConfiguration;
-import java.util.Collection;
+import com.metservice.kanban.model.WorkItemType;
+import java.io.File;
+import java.io.FileInputStream;
 import javax.servlet.http.HttpServletResponse;
+import org.apache.commons.io.IOUtils;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class KanbanDataController {
@@ -20,34 +23,22 @@ public class KanbanDataController {
         this.kanbanService = new KanbanService();
     }
 
-    @RequestMapping("/download-csv")
-    public void downloadCsv(@PathVariable("projectName") String projectName, 
+    public KanbanDataController(KanbanService kanbanService) {
+        this.kanbanService = kanbanService;
+    }
+    
+    @RequestMapping("/download/{projectName}")
+    public void download(@PathVariable("projectName") String projectName, 
+        @RequestParam("workItemTypeName") String workItemTypeName,
         HttpServletResponse response) throws FileNotFoundException, IOException {
         
-        //TODO:Test bad project name
-        
         KanbanProjectConfiguration configuration = kanbanService.getProjectConfiguration(projectName);
-        KanbanPersistence persistence = new KanbanPersistence(configuration);
+        WorkItemType workItemType = configuration.getWorkItemTypes().getByName(workItemTypeName);
         
-        
-        
-        Collection<KanbanCsvFile> files = persistence.getFiles();
-        
-        
-        
-        if (files.size() == 1) {
-            //TODO: Test single level project
-            response.setContentType("text/csv");
-        } else if (files.size() > 1) {
-            //TODO: Test multi level project
-            response.setContentType("application/zip");
-        } else {
-            //TODO: Is this possible if you've found a valid project?
-        }
-        
-//        response.setContentLength(file.getFile().length);
-//        response.setHeader("Content-Disposition","attachment; filename=\"" + file.getFilename() +"\"");
-//        IOUtils.copy(zipFile, response.getOutputStream());
-              
+        File file = configuration.getDataFile(workItemType);
+        response.setContentType("text/csv");
+        response.setContentLength((int)file.length());
+        response.setHeader("Content-Disposition","attachment; filename=\"" + file.getName() +"\"");
+        IOUtils.copy(new FileInputStream(file), response.getOutputStream());
     }
 }
